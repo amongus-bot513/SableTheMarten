25b5eb9ffe7286c654f11cdd9ba55811d2a2ea3e
amongus-bot513 <tyrkea36@kingshigh.school.nz>
2021-06-01 10:54:23 +1200

    added jump and double jump

--- player.gd
+++ player.gd
@@ -1,18 +1,21 @@
 extends KinematicBody
  
 
-var gravity = Vector3.DOWN * 12
+#var gravity = Vector3.DOWN * 12
 var speed = 10
-var jump_speed = 6
 
 var velocity = Vector3()
+var jumpforce : float = 10.0
+var gravity : float = 17.0
+var on_ground = false
+var has_double_jumped = false
+
+
 
 func _physics_process(delta):
-	velocity += gravity * delta
-	get_input()
-	velocity = move_and_slide(velocity, Vector3.UP)
-
-func get_input():
+	velocity.y -= gravity * delta
+	#get_input()
+	
 	velocity.x = 0
 	velocity.z = 0
 	if Input.is_action_pressed("move_forward"):
@@ -23,3 +26,38 @@ func get_input():
 		velocity.x += speed 
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= speed 
+	
+	if is_on_floor():
+		on_ground = true
+	else:
+		on_ground = false
+	#if Input.is_action_pressed("jump") and is_on_floor():
+		#velocity.y = jumpforce
+	if Input.is_action_just_pressed("jump"):
+		if on_ground == true:
+			velocity.y = jumpforce
+			has_double_jumped = false
+			on_ground = false
+		elif on_ground == false and has_double_jumped == false:
+			velocity.y = jumpforce
+			has_double_jumped = true
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	
+	
+	
+
+#func get_input():
+	#velocity.x = 0
+	#velocity.z = 0
+	#if Input.is_action_pressed("move_forward"):
+		#velocity.z -= speed 
+	#if Input.is_action_pressed("move_backward"):
+		#velocity.z += speed 
+	#if Input.is_action_pressed("move_right"):
+		#velocity.x += speed 
+	#if Input.is_action_pressed("move_left"):
+		#velocity.x -= speed 
+	
+	
